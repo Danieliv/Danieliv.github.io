0387099beee7acbde1c61ab9b404decc49e10060
Daniel Navarrete <11926357+Danieliv@users.noreply.github.com>
2024-04-05 16:39:24 -0700

    Introduced new service method 'fetch_forecast_by' in order to consume the forecast API

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -5,8 +5,13 @@ class WeatherService
   format   :json
   headers "Content_type" => "application/json"
 
-  def self.fetch_weather_by(zip:, city:, latitude:, longitude:)
+  def self.fetch_weather_by(latitude:, longitude:, city:, zip:)
     response = get("/weather?lat=#{latitude}&lon=#{longitude}&q=#{city}&zip=#{zip}&units=imperial&appid=#{ENV['OPENWEATHERMAP_API_KEY']}")
+    JSON.parse(response.body)
+  end
+
+  def self.fetch_forecast_by(latitude:, longitude:, city:, zip:, count:)
+    response = get("/forecast?lat=#{latitude}&lon=#{longitude}&q=#{city}&zip=#{zip}&cnt=#{count}&units=imperial&appid=#{ENV['OPENWEATHERMAP_API_KEY']}")
     JSON.parse(response.body)
   end
 end
